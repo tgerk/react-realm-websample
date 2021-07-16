92983a5afd0e84d7b53926e4210c006b282fc30a
Tim Gerk <tjgerk@gmail.com>
2021-07-16 10:58:17 -0700

    reorganize for splitting a subtree
    want a subtree for only the React app

--- realm/Application-1/functions/cuisinesResolver.js
+++
@@ -1,7 +0,0 @@
-
-exports = async (input) => {
-  const db = context.services.get("mongodb-atlas").db("sample_restaurants");
-  const cuisines = await db.collection("restaurants").distinct("cuisine");
-
-  return cuisines;
-};
